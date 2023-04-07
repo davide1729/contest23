5d56d4366c7db06ec469acd81ad48216cdff3763
davide1729 <arnaldodabrescia90@gmail.com>
2023-04-07 11:46:56 +0200

    pagerank sol draft

--- pagerank/solutions/pagerank.py
+++ pagerank/solutions/pagerank.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+
+# soluzione pagerank
+# Demetrio e Davide
+
 import random #random generator
 import networkx as nx #this is for network
 import matplotlib.pyplot as plt #this is for drawing
@@ -7,67 +12,67 @@ import time # this is to get computational time
 import pandas as pd # this is for dataframes
 import math # some math tools for week 5
 
-number_of_nodes = 5
-
-neighbors = {0:[1,3,4],
+'''neighbors = {0:[1,3,4],
              1:[0,2,3,4],
              2:[1,4],
              3:[0,1,4],
              4:[0,1,2,3]
-             }
+             }'''
 
+def pagerank():
+    mypr = {}
+    k_out = {}
+    #N = number_of_nodes    
 
+    for i in neighbors:
+        mypr[i]=1/N 
+        k_out[i]=len(neighbors[i])
 
-mypr = {}
-k_out = {}
-N = number_of_nodes
+    iterations = 0  
+    a = 0.15
+    while True:
+        diff = 0
+        mypr2 = mypr.copy() # we create a clone of mypr in order to distinguish two contiguous iterations
+        for i in mypr2:
+            sum = 0
+            for j in neighbors[i]:
+                sum += mypr2[j]/k_out[j]        
+            mypr2[i]= a/N + (1-a)*sum
+        print(mypr2[1])    
+        iterations += 1 # iteration counter
 
-
-    
-
-for i in neighbors:
-    mypr[i]=1/N 
-    k_out[i]=len(neighbors[i])
-
-# NEW APPROACH
-
-iterations = 0  
-a = 0.15
-while True:
-    diff = 0
-    mypr2 = mypr.copy() # we create a clone of mypr in order to distinguish two contiguous iterations
-    for i in mypr2:
-        sum = 0
-        for j in neighbors[i]:
-            sum += mypr2[j]/k_out[j]        
-        mypr2[i]= a/N + (1-a)*sum
-    print(mypr2[1])    
-    iterations += 1 # iteration counter
-
-    for i in mypr2:
-        diff = abs(mypr[i] - mypr2[i]) # we check if *all* the values are the same
-        if diff < 10**-6: # in order to check the convergence between two iterations
-            a = 1 
+        for i in mypr2:
+            diff = abs(mypr[i] - mypr2[i]) # we check if *all* the values are the same
+            if diff < 10**-6: # in order to check the convergence between two iterations
+                a = 1 
+            
+        if a == 1:
+            break
         
-    if a == 1:
-        break
-    
-    mypr = mypr2
-    
-print("Number of iterations:",iterations)
-print(mypr)
-max = 0
-for i in mypr:
-    if i+1 >= len(mypr):
-        continue
-    else:
-        if mypr[i+1]>mypr[i]:
-            max = i+1
+        mypr = mypr2
+
+    max = 0
+    for i in mypr:
+        if i+1 >= len(mypr):
+            continue
+        else:
+            if mypr[i+1]>mypr[i]:
+                max = i+1
+
+T = int(input())
+a0 = int(input())
+a = a0/10
+
+for t in range(T):
+    input()
+    N = int(input())
+    neighbors = {}
+    for node in N:
+        neighbors[node] = []
+        
 
 
-print(max)
 
-    
 
 
 #######################
