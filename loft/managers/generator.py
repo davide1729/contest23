648a8d75a479853b1be60534aaf1e6439f3bbe8d
fraasa <125486914+fraasa@users.noreply.github.com>
2023-04-08 17:12:38 +0200

    Co-authored-by: Leonardo Azzi <ItsMrLeo@users.noreply.github.com>

--- loft/managers/generator.py
+++ loft/managers/generator.py
@@ -1,32 +1,62 @@
 import random
 
-def generate_test_cases(num_cases, min_len, max_len):
-    test_cases = []
-    commands = ['{_}', '{^}', '{<}', '{>}']
-    
-    for _ in range(num_cases):
-        input_len = random.randint(min_len, max_len)
-        input_string = r""
-        
-        for _ in range(input_len):
-            if random.random() < 0.5:   # Adjust the probability of digits
-                                        #vs. commands (0.5 should mean 50% chance of a digit)
-                input_string += str(random.randint(1, 9))
-            else:
-                input_string += random.choice(commands)
-        
-        test_cases.append(input_string)
-    
-    return test_cases
 
-num_cases = 5
-min_len = 10
-max_len = 30
+# Define commands to be used in the test cases:
+commands = ['{_}', '{^}', '{<}', '{>}']
 
-test_cases = generate_test_cases(num_cases, min_len, max_len)
-test_dict = {}
-for i, test_case in enumerate(test_cases):
-    test_dict[f"Test Case {i}"] = test_case
-    print(f"Test case {i + 1}: {test_case}")
+# Generator parameters:
 
-print(test_dict)
+easy_range = (1, 50)
+
+medium_range = (50, 100)
+
+hard_range = (100, 500)
+
+commands_range = (50, 100)
+
+
+# Generate easy test cases:
+def easy_case():
+    input_len = random.randint(easy_range[0], easy_range[1])
+    input_string = ''.join(random.choices(commands + [str(i) for i in range(1, 10)], k=input_len))
+    return input_string
+
+# Generate medium test cases:
+def medium_case():
+    input_len = random.randint(medium_range[0], medium_range[1])
+    input_string = ''.join(random.choices(commands + [str(i) for i in range(1, 10)], k=input_len))
+    return input_string
+
+# Generate hard test cases:
+def hard_case():
+    input_len = random.randint(hard_range[0], hard_range[1])
+    input_string = ''.join(random.choices(commands + [str(i) for i in range(1, 10)], k=input_len))
+    return input_string
+
+# Generate edge cases:
+
+def edge_case_1():
+    # Empty input string
+    return "{}"
+
+def edge_case_2():
+    # 1 Command repeated multiple times : maybe not necessary
+    num_commands = random.randint(commands_range[0], commands_range[1])
+    command_string = random.choice(commands)
+    input_string = command_string * num_commands
+    return input_string
+
+def edge_case_3():
+    # Cursor moving out of bounds
+    input_len = random.randint(commands_range[0], commands_range[1])
+    input_string = ''.join(random.choices(commands, k=input_len))
+    return input_string + random.choice(commands)
+
+CASES = [easy_case]*4 + [medium_case]*6 + [hard_case]*6 + [edge_case_1]*2 + [edge_case_2]*4 + [edge_case_3]*3
+
+print(len(CASES))
+
+for x in CASES:
+    print()
+    print(x())
+
