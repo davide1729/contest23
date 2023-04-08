41c11311cba557b704cff167774ceb4bc3df5426
Mturco3 <michele.turco3@gmail.com>
2023-04-08 13:40:15 +0200

    Fantacoso tentative solutions

---
+++ fantacoso/michele.py
@@ -0,0 +1,32 @@
+"""
+@author: Michele Turco
+
+Task: fantacoso
+"""
+
+T=int(input())
+
+for t in range(1,T+1):
+    ln=input()
+    ln=input().split()
+    N=int(ln[0])
+    m=int(ln[1])
+    
+    w=[0]
+    p=[0]
+    
+    
+    g=N
+    h=m
+    ris=[]
+ 
+    print("Case #{}: ".format(t), end="")
+    for e in ris:
+        print(e, end=" ")
+    print()
+
+
+
+
+
+
