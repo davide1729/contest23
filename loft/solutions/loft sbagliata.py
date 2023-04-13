7a800831d204fd2fad8b0dec6b93b5051ab17879
fraasa <125486914+fraasa@users.noreply.github.com>
2023-04-13 16:05:45 +0200

    Update loft sbagliata.py

--- loft/solutions/loft sbagliata.py
+++ loft/solutions/loft sbagliata.py
@@ -31,12 +31,7 @@ def decoder(test):
             cursor_index[1] = min(len(matrix.get(cursor_index[0], [])), cursor_index[1])
         elif i == '>':
             cursor_index[1] = min(len(matrix.get(cursor_index[0], [])), cursor_index[1] + 1)
-        elif i == '<':
-            cursor_index[1] = max(0, cursor_index[1] - 1)
-        elif i.isdigit():
-            matrix[cursor_index[0]] = matrix.get(cursor_index[0], [])
-            matrix[cursor_index[0]].insert(cursor_index[1], int(i))
-            cursor_index[1] += 1
+        
     result = []
     for n in range(len(matrix)):
         result.append(matrix.get(n, []))
